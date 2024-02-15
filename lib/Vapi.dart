d7d5c36bd804d67d052048be4f7306a6865c2ea8
Harrison Kussie <harrison@vapi.ai>
2024-02-14 21:57:26 -0800

    basic implement of start call
    basic implement of start call

--- lib/Vapi.dart
+++ lib/Vapi.dart
@@ -10,5 +10,27 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'models/exports/Exports.dart';
 
 class Vapi {
-  int addOne(int value) => value + 1;
+  late final CallClient _callClient;
+
+  Vapi() {
+    _callClient = Daily().createCallClient(); 
+  }
+
+  Future<void> startCall(String roomUrl) async {
+    try {
+      await _callClient.join(CallJoinData(url: roomUrl));
+    } catch (e) {
+      print("Error starting call: $e");
+    }
+  }  
+  // int addOne(int value) => value + 1;
 }
+/* 
+  1. makeUrl: line 203 reference
+  2. makeUrlRequest: line 217 reference
+  3. callDidFail: line 281 reference
+  4. VapiError.customError: refer to method, VapiError, need equivalent for Swift.Error in dart
+  5. WebCallResponse, refer to method, Decodable equivalent
+  6. networkManager: refer to method, need URLSession equivalent, JSONDecoder equivalent
+  7. joinCall: line 181 reference
+*/
